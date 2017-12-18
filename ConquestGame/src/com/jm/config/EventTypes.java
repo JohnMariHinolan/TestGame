3c683b9ed3a8a670674aac93d80a57297f220e82
jhinolan <jhinolan@GBL13315>
2017-12-18 20:07:16 +0800

    Brainstorming...

---
+++ ConquestGame/src/com/jm/config/EventTypes.java
@@ -0,0 +1,19 @@
+package com.jm.config;
+/**
+ * For brain storming only
+ * Events where an ability or victory  condition will be looked
+ * **/
+public enum EventTypes {
+
+	CARD_DAMAGED,
+	CARD_DESTROYED,
+	CARD_ATTACK,
+	HERO_DAMAGE,
+	END_OF_TURN,
+	CARD_DRAW,
+	
+	;
+	
+	
+	
+}
